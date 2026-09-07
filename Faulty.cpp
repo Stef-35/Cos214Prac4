721d4dfc9951db9bebfd222c6f41389d9456f01a
Stef-35 <stefanvanstaden06@gmail.com>
2026-09-07 12:41:26 +0200

    implemented haslabel

--- Faulty.cpp
+++ Faulty.cpp
@@ -1,11 +1,18 @@
 #include "Faulty.h"
 
-double Faulty::getWeight() {
+double Faulty::getWeight()
+{
 	// TODO - implement Faulty::getWeight
 	throw "Not yet implemented";
 }
 
-string Faulty::inspect() {
+string Faulty::inspect()
+{
 	// TODO - implement Faulty::inspect
 	throw "Not yet implemented";
+}
+
+bool Faulty::hasLabel(const string &label)
+{
+	return label == "Faulty" || unit->hasLabel(label);
 }
